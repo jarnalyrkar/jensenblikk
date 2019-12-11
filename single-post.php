f05a2720038403b1a7359e34b18589321ae5d306
jarnalyrkar <joachimsolstrom@gmail.com>
2019-12-11 08:43:06 +0100

    BlueTint testing

--- single-post.php
+++ single-post.php
@@ -2,5 +2,10 @@
 $context = Timber::context();
 
 $context['post'] = new Timber\Post();
+// nextPost-permalink
+// nextPost-title
+$nextPost = new Timber\Post();
+
+var_dump($context['post']);
 
 Timber::render('pages/single-post.twig', $context);
