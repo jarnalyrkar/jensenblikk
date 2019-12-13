20878d354473cc4de32608b35a2d7e5990187d23
jarnalyrkar <joachimsolstrom@gmail.com>
2019-12-13 11:45:49 +0100

    Fix Referance page (single-post), and gallery

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,6 @@
 import MobileMenu from './modules/MobileMenu';
-import SetGalleryHeight from './modules/SetGalleryHeight';
 
 const mobileMenu = new MobileMenu();
-const setGalleryHeight = new SetGalleryHeight();
 //import bootstrap from '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min';
 //import bootstrapSlim from '../../node_modules/jquery/dist/jquery.slim.min';
 
--- single-post.php
+++ single-post.php
@@ -1,11 +1,5 @@
 <?php
 $context = Timber::context();
-
 $context['post'] = new Timber\Post();
-// nextPost-permalink
-// nextPost-title
-$nextPost = new Timber\Post();
-
-var_dump($context['post']);
 
 Timber::render('pages/single-post.twig', $context);
